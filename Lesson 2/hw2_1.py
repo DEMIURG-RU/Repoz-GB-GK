d793e5a86850c5889ed729f2d4afddb1af390131
DEMIURG <demiurg-ru@rambler.ru>
2021-10-12 15:43:48 +0300

    Закончил первое задание второго урока.

---
+++ Lesson 2/hw2_1.py
@@ -0,0 +1,5 @@
+my_list = [16, 3.14, 'Vova', True, [4, 7], (2, 4, 7, 3.6), {7: 'Vova', 8: 'Doka'}]
+l_ght = len(my_list)
+#print(l_ght) ghjdthrf
+for t in range(l_ght):
+    print(f'{t} {type(my_list[t])}')
