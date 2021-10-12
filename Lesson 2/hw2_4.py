4e0338202cedfe670ff205007c2226e2df4645c6
DEMIURG <demiurg-ru@rambler.ru>
2021-10-12 15:46:53 +0300

    Четвертое задание второго урока

---
+++ Lesson 2/hw2_4.py
@@ -0,0 +1,10 @@
+my_str = 'Мне нравяться сложные и интересные головоломки'
+my_rstr = my_str.split()
+ch_el = len(my_rstr)
+for t in range(0, ch_el):
+    ch_w = len(my_rstr[t])
+    if ch_w <= 10:
+        print(f'{t+1} {my_rstr[t]}')
+    else:
+        b = my_rstr[t]
+        print(f'{t+1} {b[0:10]}')
