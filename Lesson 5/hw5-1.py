e14321c46f286a79ce7e1504a1930521aa358427
DEMIURG <demiurg-ru@rambler.ru>
2021-10-24 09:42:02 +0300

    Lesson 5 task 2

---
+++ Lesson 5/hw5-1.py
@@ -0,0 +1,4 @@
+my_f = open('my_prime.doc', 'r')
+content = my_f.readline()
+print(content)
+my_f.close
---
+++ Lesson 5/hw5_11.py
@@ -0,0 +1,5 @@
+op_f = open('l_f.txt', 'w')
+str_list = ['Я туту был\n', 'Я тут буду\n', 'Я здесь сейчас\n']
+op_f.writelines(str_list)
+op_f.close()
+
